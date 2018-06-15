1748b5cc09538f8f39a25e7c1d98cc6501558539
mjafri118 <31643580+mjafri118@users.noreply.github.com>
2018-06-15 14:28:14 -0400

    EAAlert old code cleanup

--- src/HomePage/assets/EAButton/EAAlert.js
+++ src/HomePage/assets/EAButton/EAAlert.js
@@ -10,28 +10,6 @@ class EAAlert extends React.Component {
         this.CreateAlert = this.CreateAlert.bind(this);
     }
 
-    // handleSubmit(e){
-    //     axios.post('http://eng-cache-198419.appspot.com/create_user',{
-    //         name: this.state.name,
-    //         email: this.state.email
-    //     })
-    //     .then(function (response){
-    //         var success = (response.data.statusCode === 200 ? "Success" : "Failure");
-    //         console.log(success);
-    //
-    //     })
-    //     .catch(function (error){
-    //         console.log(error)
-    //     })
-    //     e.preventDefault();
-    // }
-    //
-    // handleChange(e) {
-    //     e.preventDefault();
-    //     const { name, value } = e.target;
-    //     this.setState({ [name]: value });
-    // }
-
     CreateAlert(props){
         if (this.props.success === true)
             return <Alert bsStyle='success'>Success! You have been signed up.</Alert>;
